f3cf349928c7b9da7bb73c4b6f94d5d1e3f161d7
Austin Wong <38020337+mimo1112@users.noreply.github.com>
2024-02-04 04:30:50 -0800

    mvp done

--- script.js
+++ script.js
@@ -1,102 +1,111 @@
-const noButton = document.getElementById('no');
+const noButton = document.getElementById("no");
 let canClick = true;
 let inCooldown = false;
-const yesButton = document.getElementById('yes');
+const yesButton = document.getElementById("yes");
+const wholeBackground = document.querySelector(".whole");
 
-noButton.addEventListener('mouseover', function () {
-    noButton.style.left = `${Math.floor(Math.random() * 200)}%`;
-    noButton.style.top = `${Math.floor(Math.random() * 200)}%`;
+noButton.addEventListener("mouseover", function () {
+  noButton.style.left = `${Math.floor(Math.random() * 200)}%`;
+  noButton.style.top = `${Math.floor(Math.random() * 200)}%`;
 });
 
-noButton.addEventListener('click', function () {
-    if (canClick && !inCooldown) {
-        changeNoText();
-        canClick = false;
+noButton.addEventListener("click", function () {
+  if (canClick && !inCooldown) {
+    changeNoText();
+    canClick = false;
 
-        setTimeout(function () {
-            canClick = true;
-        }, 2000);
+    setTimeout(function () {
+      canClick = true;
+    }, 2000);
 
-        noButton.style.left = `${Math.floor(Math.random() * 200)}%`;
-        noButton.style.top = `${Math.floor(Math.random() * 200)}%`;
-    }
-    var rand = Math.floor(Math.random() * 7);
-    var str = "img/no/" + rand + ".gif";
+    noButton.style.left = `${Math.floor(Math.random() * 200)}%`;
+    noButton.style.top = `${Math.floor(Math.random() * 200)}%`;
+  }
+  var rand = Math.floor(Math.random() * 7);
+  var str = "img/no/" + rand + ".gif";
 
-    replaceNoImage(str);
+  replaceNoImage(str);
 });
 
 function replaceNoImage(imagePath) {
-    const imgBg = document.querySelector('.img-bg');
-    const newTxt = document.querySelector('.txt');
+  const imgBg = document.querySelector(".img-bg");
+  const newTxt = document.querySelector(".txt");
 
-    imgBg.src = imagePath;
-    imgBg.width = 400;
-    imgBg.height = 400;
+  imgBg.src = imagePath;
+  imgBg.width = 400;
+  imgBg.height = 400;
 }
 
 function changeNoText() {
-    var noButton = document.getElementById('no');
-    noButton.innerText = randomNoPhrases();
+  var noButton = document.getElementById("no");
+  noButton.innerText = randomNoPhrases();
 }
 
 let lastRandomIndex;
 
 function randomNoPhrases() {
-    const phrases = ["Are you sure?",
-        "Come on Babe? :(",
-        "You can't do this :(",
-        "Please Babe?",
-        "Think about all the fun we could have!"
-    ];
+  const phrases = [
+    "Are you sure?",
+    "Come on Babe? :(",
+    "You can't do this :(",
+    "Please Babe?",
+    "Think about all the fun we could have!",
+  ];
 
-    let rand;
+  let rand;
 
-    do {
-        rand = Math.floor(Math.random() * phrases.length);
-    } while (rand === lastRandomIndex);
+  do {
+    rand = Math.floor(Math.random() * phrases.length);
+  } while (rand === lastRandomIndex);
 
-    lastRandomIndex = rand;
+  lastRandomIndex = rand;
 
-    return phrases[rand];
+  return phrases[rand];
 }
 
-yesButton.addEventListener('click', function () {
-    var rand = Math.floor(Math.random() * 7);
-    var str = "img/yes/" + rand + ".gif";
+yesButton.addEventListener("click", function () {
+  var rand = Math.floor(Math.random() * 7);
+  var str = "img/yes/" + rand + ".gif";
 
-    replaceYesImage(str);
+  replaceYesImage(str);
+
+  wholeBackground.style.backgroundImage = `url(img/main/123.gif)`;
 });
 
 function replaceYesImage(imagePath) {
-    const imgBg = document.querySelector('.img-bg');
-    const newTxt = document.querySelector('.txt');
-    const audio = document.querySelector('audio');
+  const imgBg = document.querySelector(".img-bg");
+  const newTxt = document.querySelector(".txt");
+  const audio = document.querySelector("audio");
 
-    imgBg.src = imagePath;
-    imgBg.width = 400;
-    imgBg.height = 400;
-    yesButton.style.display = 'none';
-    noButton.style.display = 'none';
-    newTxt.innerText = txtPhrases();
-    audio.play();
+  imgBg.src = imagePath;
+  imgBg.width = 400;
+  imgBg.height = 400;
+  yesButton.style.display = "none";
+  noButton.style.display = "none";
+  newTxt.innerText = txtPhrases();
+  audio.play();
 }
 
 function txtPhrases() {
-    const phrases = ["HEHEHEHEHE",
-        "I LOVE YOU\u2764",
-        "Thanks Babe \uD83D\uDE0A",
-        "사랑해요\u2764"
-    ];
-    var rand = Math.floor(Math.random() * phrases.length);
-    return phrases[rand];
+  const phrases = [
+    "HEHEHEHEHE",
+    "I LOVE YOU\u2764",
+    "Thanks Babe \uD83D\uDE0A",
+    "사랑해요\u2764",
+    "I love you and I miss you with all my heart\u2764",
+  ];
+  var rand = Math.floor(Math.random() * phrases.length);
+  return phrases[rand];
 }
-document.addEventListener('DOMContentLoaded', function () {
-    var audio = document.querySelector('audio');
-    
-    audio.addEventListener('ended', function () {
-        audio.currentTime = 0;
-        audio.play();
-    });
+document.addEventListener("DOMContentLoaded", function () {
+  var audio = document.querySelector("audio");
+
+  audio.addEventListener("ended", function () {
+    audio.currentTime = 0;
+    audio.volume = 0.1;
     audio.play();
+  });
+  audio.volume = 0.1;
+  audio.play();
 });
+wholeBackground.style.backgroundImage = "none";
